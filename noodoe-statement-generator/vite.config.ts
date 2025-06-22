045d9e662a02819846f2d181f6c4f3e6a9aedbe4
aiden <aidenmods123@gmail.com>
2025-06-22 19:42:15 -0400

    updaed gh pages url

--- noodoe-statement-generator/vite.config.ts
+++ noodoe-statement-generator/vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
 export default defineConfig({
-  base: '/noodoe-statement-generator/',
+  base: '/statement-generator/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
